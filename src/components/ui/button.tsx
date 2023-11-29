8f5a6682f9d43b198e67feda5ea604446ecb9b84
ananducherakkal <ananducherakkal.dev@gmail.com>
2023-11-29 08:49:36 +0100

    Initial setup

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/utils/shadcn";
+import * as React from "react";
+
+const buttonVariants = {
+  default: "",
+  variant: {
+    default:
+      "bg-primary text-text border-2 border-primary hover:bg-primary/80 hover:border-primary/80 active:bg-transparent active:text-primary rounded-md",
+    secondary:
+      "bg-gray-70 text-text-light-secondary border-2 border-gray-70 hover:bg-gray-70/80 hover:border-gray-70/80 active:bg-transparent active:text-gray-70 rounded-md",
+    "outline-light":
+      "bg-transparent text-text-light border-2 border-text-light-secondary hover:border-text-light active:opacity-100 active:bg-text-light/20 rounded-md",
+    outline:
+      "bg-transparent text-text dark:text-text-light border-2 border-text-secondary dark:border-text-light-secondary hover:border-text dark:hover:border-text-light active:opacity-100 active:bg-text/20 dark:active:bg-text-light/20 rounded-md",
+    transparent:
+      "bg-transparent text-text-light-secondary hover:text-text-light active:text-text-light/80",
+  },
+  size: {
+    default: "h-14 text-base px-4 py-2 font-semibold",
+    sm: "h-6 px-3 min-w-[48px] rounded-xl text-xs",
+  },
+};
+
+export interface IButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: keyof (typeof buttonVariants)["variant"];
+  size?: keyof (typeof buttonVariants)["size"];
+}
+
+export default function Button(props: IButtonProps) {
+  const { children, className, variant = "default", size = "default" } = props;
+
+  const defaultClassName = buttonVariants.default;
+  const variantClassName = buttonVariants.variant[variant];
+  const sizeClassName = buttonVariants.size[size];
+
+  return (
+    <button
+      className={cn(
+        defaultClassName,
+        variantClassName,
+        sizeClassName,
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,111 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          DEFAULT: "rgb(var(--primary-50) / <alpha-value>)",
+          foreground: "rgb(var(--text) / <alpha-value>)",
+        },
+        secondary: {
+          DEFAULT: "rgb(var(--gray-60) / <alpha-value>)",
+          foreground: "rgb(var(--text) / <alpha-value>)",
+        },
+        background: {
+          DEFAULT: "rgb(var(--bg-80) / <alpha-value>)",
+          light: "rgb(var(--bg-30) / <alpha-value>)",
+        },
+        border: {
+          DEFAULT: "rgb(var(--border-50) / <alpha-value>)",
+          50: "rgb(var(--border-50) / <alpha-value>)",
+          30: "rgb(var(--border-30) / <alpha-value>)",
+          10: "rgb(var(--border-10) / <alpha-value>)",
+        },
+        gray: {
+          10: "rgb(var(--gray-10) / <alpha-value>)",
+          30: "rgb(var(--gray-30) / <alpha-value>)",
+          40: "rgb(var(--gray-40) / <alpha-value>)",
+          60: "rgb(var(--gray-60) / <alpha-value>)",
+          70: "rgb(var(--gray-70) / <alpha-value>)",
+          80: "rgb(var(--gray-80) / <alpha-value>)",
+        },
+        text: {
+          DEFAULT: "rgb(var(--text) / <alpha-value>)",
+          secondary: "rgb(var(--text-secondary) / <alpha-value>)",
+          light: {
+            DEFAULT: "rgb(var(--text-light) / <alpha-value>)",
+            secondary: "rgb(var(--text-light-secondary) / <alpha-value>)",
+          },
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        // shadcn
+        input: "var(--bg-50)",
+        ring: "var(--primary-50)",
+        foreground: "hsl(var(--foreground))",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontSize: {
+        xl: "32px",
+        lg: "20px",
+        md: "18px",
+        base: "16px",
+        sm: "14px",
+        xs: "12px",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
